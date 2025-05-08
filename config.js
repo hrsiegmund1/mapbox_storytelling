590cb2952f81458755ef4cdb21e725618fb0daa5
Hannah Siegmund <siegmuhr@dukes.jmu.edu>
2025-05-08 18:53:04 -0400

    new image dog cover first slide

--- config.js
+++ config.js
@@ -186,7 +186,7 @@ description: "Shug Avery’s arrival disrupts the household's dynamic and opens 
     alignment: 'centered',
     hidden: false,
     title: 'Salvage the Bones: Introduction',
-    image: 'Salvage the Bones.jpg',  // Make sure the image is in the assets folder
+    image: 'Tree Salvage Cover.jpg',  // Make sure the image is in the assets folder
     description: "Jesmyn Ward’s Salvage the Bones introduces Esche, a young girl growing up in the South in the days leading up to Hurricane Katrina. Esche has a journey to self empowerment and displays women's agency",
     location: {
         center: [-89.26344602946764, 30.37877669386771],  // Coordinates for the rural Mississippi setting (the novel's setting)

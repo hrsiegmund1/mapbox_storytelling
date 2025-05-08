717ed581bc2cccf8557da696a9bffe350f167566
Hannah Siegmund <siegmuhr@dukes.jmu.edu>
2025-05-08 10:30:05 -0400

    New subtitle change

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ var config = {
     theme: 'light',
     use3dTerrain: false, //set true for enabling 3D maps.
     auto: false,
-    title: 'Agency Mapped: Southern Women in Literature',
+    title: 'Agency Mapped: Women in Southern Literature',
     subtitle: 'By using spatial movement and economic data across across The Color Purple, Their Eyes Were Watching God, and Salvage the Bones, greater themes of how Southern women assert agency are displayed.',
     byline: 'Hannah Siegmund',
     footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',

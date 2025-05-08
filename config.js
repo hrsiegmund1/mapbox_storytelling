37753d8d91b103581f1e290490cdc348bec996df
Hannah Siegmund <siegmuhr@dukes.jmu.edu>
2025-05-08 17:24:35 -0400

    Edit to Muck sentence

--- config.js
+++ config.js
@@ -168,7 +168,7 @@ description: "Shug Avery’s arrival disrupts the household's dynamic and opens 
             hidden: false,
             title: 'Their Eyes Were Watching God: The Muck',
             image: 'The Everglades.jpeg',  // Make sure to upload the appropriate image for this chapter
-            description: "Janie and Tea Cake move to the Everglades, where they work in the muck, cultivating land and living in a community of migrant workers. This chapter marks a significant shift in Janie's journey toward agency. In the muck, she experiences true **freedom** for the first time, shedding the constraints placed on her in Eatonville and with Joe. Here, Janie finds joy in hard work, companionship, and self-expression. The muck is where Janie truly learns what it means to live on her own terms, contributing to her growing sense of **empowerment**.",
+            description: "Janie and Tea Cake move to the Everglades, where they work in the muck. This chapter marks a significant shift in Janie's journey toward agency. In the muck, she experiences true freedom and pure love for the first time, a significant difference from Joe. Here, Janie finds happiness and self-expression. The muck is where Janie truly learns what it means to live on her own terms, contributing to her growing sense of empowerment and independence.",
             location: {
                 center: [-81.38478, 25.85764],  // Coordinates for the muck in the Everglades
                 zoom: 10,
